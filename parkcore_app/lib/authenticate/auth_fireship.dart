d2e425d0d3ceaddb2ce53b2814155e745d21424b
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-03-29 10:58:17 -0700

    merged with master - login/authentication

---
+++ parkcore_app/lib/authenticate/auth_fireship.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AuthService {
+  //constructor 
+  AuthService() {
+    user = Observable(_auth.onAuthStateChanged);
+
+    profile = user.switchMap((FirebaseUser u) {
+      if (u != null) {
+        return _db
+            .collection('users')
+            .document(u.uid)
+            .snapshots()
+            .map((snap) => snap.data);
+      } else {
+        return Observable.just({});
+      }
+    });
+  }
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+
+  Observable<FirebaseUser> user; //firebase user
+  Observable<Map<String, dynamic>> profile;
+  PublishSubject loading = PublishSubject();
+
+  Future<FirebaseUser> googleSignIn() async {
+    loading.add(true);
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.idToken,
+    );
+    FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+
+    updateUserData(user);
+    print("signed in " + user.displayName);
+    loading.add(false);
+    return user;
+  }
+
+  void updateUserData(FirebaseUser user) async {
+    DocumentReference ref = _db.collection('users').document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'email': user.email,
+      'photoURL': user.photoUrl,
+      'displayName': user.displayName,
+      'lastSeen': DateTime.now()
+    }, merge: true);
+  }
+
+  void signOut() {
+    _auth.signOut();
+  }
+}
+
+final AuthService authService = AuthService();
