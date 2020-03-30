5a9b0a228506bf02eba75f866b218954744cd5f9
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-03-30 14:18:41 -0700

    add user's name to form (uid saved to DB)

--- parkcore_app/lib/parking/add_parking.dart
+++ parkcore_app/lib/parking/add_parking.dart
@@ -33,6 +33,21 @@ class _MyAddParkingState extends State<AddParking> {
   // Note: This is a `GlobalKey<FormState>`, not GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+  FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCurrentUser();
+  }
+
+  void _loadCurrentUser() {
+    FirebaseAuth.instance.currentUser().then((FirebaseUser user) {
+      setState(() {
+        this.currentUser = user;
+      });
+    });
+  }
 
   int _page = 1;
   bool _incomplete = false;
@@ -66,6 +81,22 @@ class _MyAddParkingState extends State<AddParking> {
 
   File _imageFile;
   String _downloadURL;
+
+  String getUserName() {
+    if (currentUser != null) {
+      return currentUser.displayName;
+    } else {
+      return "no current user";
+    }
+  }
+
+  String getUserID() {
+    if (currentUser != null) {
+      return currentUser.uid;
+    } else {
+      return "no current user";
+    }
+  }
 
   final _stateData = [
     {"display": "California", "value": "CA"},
@@ -114,12 +145,6 @@ class _MyAddParkingState extends State<AddParking> {
     {"display": "Friday", "value": "FRI"},
     {"display": "Saturday", "value": "SAT"},
   ];
-
-  // Get the UID of the current user
-  Future<String> getUser() async {
-    FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    return user.uid;
-  }
 
   // Select an image via gallery or camera
   Future<void> getImage(ImageSource source) async {
@@ -208,6 +233,15 @@ class _MyAddParkingState extends State<AddParking> {
 
   List<Widget> buildAddress() {
     return [
+      Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.green[50],
+        ),
+        child: Text('Adding a parking space owned by: ' + getUserName()),
+      ),
+      SizedBox(height: 20),
       getTitle(),
       SizedBox(height: 10),
       getAddress(),
@@ -701,7 +735,9 @@ class _MyAddParkingState extends State<AddParking> {
       Text('Available Starting at: ' + _startTime),
       Text('Available Until: ' + _endTime),
       Text('Price per month: \$' + _price),
-      Text('Generated info:'),
+      SizedBox(height: 10),
+      Text('Additional info connected to this parking space:'),
+      Text('Parking Space Owner: ' + getUserName()),
       Text('Parking Space Coordinates: ' + _coordinates),
       SizedBox(height: 10),
     ];
@@ -803,7 +839,7 @@ class _MyAddParkingState extends State<AddParking> {
       'monthprice': _price,
       'coordinates': _coordinates,
       'downloadURL': _downloadURL,
-      'uid': await getUser(),
+      'uid': getUserID(),
     };
 
     await Firestore.instance.runTransaction((transaction) async {
@@ -838,7 +874,7 @@ class _MyAddParkingState extends State<AddParking> {
     }
     if (!_invalidLoc) {
       if (validateAndSave()) {
-        //print("User: " + await getUser());
+       // print("User: " + _displayName);
       } else {
         setState(() {
           _errorMessage = "Make sure to fill out all required fields";
