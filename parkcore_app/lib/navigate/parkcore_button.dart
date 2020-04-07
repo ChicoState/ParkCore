67e5100b9001b43ce9ed65ac288e76001d64c040
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-07 14:10:05 -0700

    Adds parkcore logo for appbar

---
+++ parkcore_app/lib/navigate/parkcore_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LogoButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: SvgPicture.asset(
+        'assets/ParkCore_WHITE_SQUIRREL_ONLY.svg',
+        fit: BoxFit.fitWidth,
+        semanticsLabel: 'ParkCore Logo',
+      ),
+      onPressed: () {
+        Navigator.pushReplacementNamed(context, '/home');
+      },
+    );
+  }
+}
