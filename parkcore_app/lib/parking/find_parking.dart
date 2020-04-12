5c1d46a7c7238f89147322bdcff8a037bea43eb7
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-12 11:41:38 -0700

    adjusted alignment, list height based on media device

--- parkcore_app/lib/parking/find_parking.dart
+++ parkcore_app/lib/parking/find_parking.dart
@@ -70,34 +70,38 @@ class _MyFindParkingState extends State<FindParking> {
   final Map<MarkerId, Marker> _markers = {};
   List<Marker> allMarkers = [];
   
-Future<void> _onMapCreated(GoogleMapController controller) async {
+  Future<void> _onMapCreated(GoogleMapController controller) async {
 
     await Firestore.instance.collection('parkingSpaces')
       .getDocuments()
       .then((QuerySnapshot snapshot) {
-    snapshot.documents.forEach((f) => 
+        snapshot.documents.forEach((f) =>
+        allMarkers.add(
+          Marker(
 
-    allMarkers.add(
-      Marker(
-        
-        markerId: MarkerId('${f.data['title']}'),
-        position: LatLng(num.parse(f.data['coordinates'].substring(1, f.data['coordinates'].indexOf(','))), num.parse(f.data['coordinates'].substring(f.data['coordinates'].indexOf(',') + 1, f.data['coordinates'].length-1))),
-        infoWindow: InfoWindow(title: '${f.data['title']}',
-        snippet: '${f.data['address']}'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen,),
+            markerId: MarkerId('${f.data['title']}'),
+            position: LatLng(
+              num.parse(f.data['coordinates'].substring(1, f.data['coordinates'].indexOf(','))),
+              num.parse(f.data['coordinates'].substring(f.data['coordinates'].indexOf(',') + 1,
+                  f.data['coordinates'].length-1)),
+            ),
+            infoWindow: InfoWindow(title: '${f.data['title']}',
+            snippet: '${f.data['address']}'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen,),
 
-      )
-    )
-    );
-  });
-  setState(() {
-    for(int i = 0; i < allMarkers.length; i++){
-      
-      _markers[allMarkers[i].markerId] = allMarkers[i];
-    
-    }
-  });
-}
+          ),
+        ),
+      );
+    });
+    setState(() {
+      for(int i = 0; i < allMarkers.length; i++){
+
+        _markers[allMarkers[i].markerId] = allMarkers[i];
+
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,59 +123,62 @@ Future<void> _onMapCreated(GoogleMapController controller) async {
     );
   }
   Widget _googlemap(BuildContext context){
-
-  return Container(
-    child: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(
-            num.parse(widget.latlong.substring(1, widget.latlong.indexOf(','))),
-            num.parse(widget.latlong.substring(widget.latlong.indexOf(',') + 1,
-            widget.latlong.length-1)),
+    return Container(
+      child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(
+              num.parse(widget.latlong.substring(1, widget.latlong.indexOf(','))),
+              num.parse(widget.latlong.substring(widget.latlong.indexOf(',') + 1,
+              widget.latlong.length-1)),
+            ),
+            zoom: 15,
           ),
-          zoom: 15,
-        ),
-        markers: _markers.values.toSet(),
-    )
-  );
-}
+          markers: _markers.values.toSet(),
+      )
+    );
+  }
 
   Widget _buildBody(BuildContext context) {
-   return StreamBuilder<QuerySnapshot>(
-     // stream: Firestore.instance.collection('parkingSpaces').snapshots(),
-    stream: Firestore.instance.collection('parkingSpaces')
+    return StreamBuilder<QuerySnapshot>(
+        // stream: Firestore.instance.collection('parkingSpaces').snapshots(),
+      stream: Firestore.instance.collection('parkingSpaces')
         .where('city', isEqualTo: widget.city)
         .snapshots(),
-     builder: (context, snapshot) {
-       if (!snapshot.hasData) return LinearProgressIndicator();
-       if(snapshot.data.documents.isEmpty){
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        if(snapshot.data.documents.isEmpty){
          return _noSpaces(context);
-       }
-       return _buildList(context, snapshot.data.documents);
-     },
-   );
- }
+        }
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
 
- Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-   return Align(
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return Align(
       alignment: Alignment.bottomCenter,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10.0),
-        height: 400.0,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.only(top: 20.0),
-          children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-        )
-      )
-   );
- }
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            height: MediaQuery.of(context).size.height/2,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              padding: const EdgeInsets.only(top: 10.0),
+              children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+            ),
+          );
+        },
+      ),
+    );
+  }
 
- Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-   
-   final parkingSpot = Spot.fromSnapshot(data);
-   
-   return Padding(
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+
+    final parkingSpot = Spot.fromSnapshot(data);
+
+    return Padding(
      //key: ValueKey(parkingSpot.address),
      padding: const EdgeInsets.all(8.0),
        child: GestureDetector(
@@ -183,60 +190,82 @@ Future<void> _onMapCreated(GoogleMapController controller) async {
             ),
           );
          },
-         child: _boxes(parkingSpot.image, parkingSpot.title, parkingSpot.city, parkingSpot.state, parkingSpot.zip,
-       parkingSpot.monthPrice, parkingSpot.type) 
+         child: _boxes(parkingSpot.image, parkingSpot.title, parkingSpot.city,
+           parkingSpot.state, parkingSpot.zip, parkingSpot.monthPrice, parkingSpot.type)
        )
     );
- }
+  }
 
- Widget _boxes(String image, String title, String city, String state, String zip, String monthprice, String type){
-    
-  return Container(
-        child: FittedBox(
-          child: Material(
-            elevation: 20.0,
-            borderRadius: BorderRadius.circular(14.0),
-            shadowColor: Color(0x802196F3),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 90,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15.0),
-                    child: Image(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(image ?? 'https://homestaymatch.com/images/no-image-available.png'),
+  Widget _boxes(String image, String title, String city, String state, String zip,
+      String monthprice, String type){
+
+    return Container(
+      child: FittedBox(
+        child: Material(
+          elevation: 20.0,
+          borderRadius: BorderRadius.circular(14.0),
+          shadowColor: Color(0x802196F3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 90,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(image ?? 'https://homestaymatch.com/images/no-image-available.png'),
+                  ),
+                ),
+              ),
+              Container(
+                width: 275,
+                height: 90,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        title ?? 'N/A',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        city + ', ' + state + ', ' + zip,
+                        style: TextStyle(fontSize: 15.0),
+                        textAlign: TextAlign.center),
+                      Text(
+                        type ?? 'N/A',
+                        style: TextStyle(fontSize: 15.0),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    '\$' + monthprice,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
-                Container(
-                  width: 275,
-                  height: 90,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(title ?? 'N/A', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0), textAlign: TextAlign.center),
-                        Text(city + ', ' + state + ', ' + zip, style: TextStyle(fontSize: 15.0), textAlign: TextAlign.center),
-                        Text(type ?? 'N/A', style: TextStyle(fontSize: 15.0), textAlign: TextAlign.center)
-                      ],
-                    ),
-                  ),
-                  ),
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text('\$' + monthprice, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
-                  ),
-                )
-                ],)
-            ),
+              ),
+            ],
           ),
-      );
+        ),
+      ),
+    );
   }
 
   Widget _noSpaces(BuildContext context){
