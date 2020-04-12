b8c4f0944e9673ac5c34ad8a5786b67ce9e79329
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-11 17:13:47 -0700

    updates default map routing

--- parkcore_app/lib/routes.dart
+++ parkcore_app/lib/routes.dart
@@ -9,6 +9,8 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   '/': (context) => LoginPage(),
   '/home': (context) => MyHomePage(title: 'ParkCore'),
   '/add_parking': (context) => AddParking(title: 'Post Your Parking Space'),
-  '/find_parking': (context) => FindParking(title: 'Find Parking'),
+  '/find_parking': (context) => FindParking(
+    title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}',
+  ),
   '/form_success': (context) => FormSuccess(),
 };
