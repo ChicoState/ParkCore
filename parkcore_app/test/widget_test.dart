564cea801e18f2f20ce95f74d60fc4f55cc6f2c4
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-04-17 09:07:43 -0700

    adds a few widget tests for home and add_parking pages

--- parkcore_app/test/widget_test.dart
+++ parkcore_app/test/widget_test.dart
@@ -10,6 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:parkcore_app/screens/home.dart';
 import 'package:parkcore_app/parking/add_parking.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
+//import 'package:geocoder/geocoder.dart';
+
 
 //void main() {
 //  testWidgets('MyHomePage has a title', (WidgetTester tester) async {
@@ -23,6 +26,7 @@ import 'package:parkcore_app/parking/add_parking.dart';
 //}
 
 void main() {
+
   testWidgets('My Home Page Widget', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
@@ -50,6 +54,7 @@ void main() {
     // Tap the search icon and trigger a frame
     await tester.tap(submit);
     await tester.pump();
+
     // Expect Text Widget message
     expect(find.text('Find parking near: Chico, CA'), findsOneWidget);
 
@@ -63,27 +68,36 @@ void main() {
 
     // Create the finders
     final titleFinder = find.text('Post Your Parking Space');
+    final Finder title = find.byKey(Key('title'));
+    final Finder address = find.byKey(Key('address'));
+    final Finder city = find.byKey(Key('city'));
+    final Finder zip = find.byKey(Key('zip'));
+
+    final Finder submit = find.widgetWithText(RaisedButton, 'Next: Parking Space Info');
+
     // Use the `findsNWidgets` matcher provided by flutter_test to
     // verify this Text widget appears exactly 2 times in the widget tree.
     expect(titleFinder, findsOneWidget);
-  });
+    expect(find.text("Part 1 of 5"), findsOneWidget);
 
-//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
-//    await tester.pumpWidget(MaterialApp(
-//      home: MyHomePage(title: "ParkCore"),
-//    ));
-//
-//    // Verify that our counter starts at 0.
-//    expect(find.text('0'), findsOneWidget);
-//    expect(find.text('1'), findsNothing);
-//
-//    // Tap the '+' icon and trigger a frame.
-//    await tester.tap(find.byIcon(Icons.add));
-//    await tester.pump();
-//
-//    // Verify that our counter has incremented.
-//    expect(find.text('0'), findsNothing);
-//    expect(find.text('1'), findsOneWidget);
-//  });
+    // expect to find 1 form
+    expect(find.byType(Form),findsOneWidget);
+    // On 1st page of form, find 4 TextFormField Widgets, 1 DropDownFormField
+    // Widget, and 1 Raised Button
+    expect(find.byType(TextFormField),findsNWidgets(4));
+    expect(find.byType(DropDownFormField),findsOneWidget);
+    expect(find.byType(RaisedButton),findsOneWidget);
+
+    // Test text field form input
+    await tester.enterText(title, '');
+    await tester.enterText(address, '');
+    await tester.enterText(city, '');
+    await tester.enterText(zip, '');
+
+    // Tap the search icon and trigger a frame
+    await tester.tap(submit);
+    await tester.pump();
+    // Expect Text Widget message
+    //expect(find.text("We can't find you!\nPlease enter a valid location."), findsOneWidget);
+  });
 }
