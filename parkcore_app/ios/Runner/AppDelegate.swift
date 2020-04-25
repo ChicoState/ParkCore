94df058f05e9f30f57a2f08a45766ce8f3591a3d
Isabel Hernandez <isabelhernandez@isabels-mbp.attlocal.net>
2020-04-25 14:24:06 -0700

    Ios API

---
+++ parkcore_app/ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Flutter
+import GoogleMaps
+import Firebase
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("YOUR_API_KEY")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
