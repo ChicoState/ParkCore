29a99ea38cda09c5048ac80fee5f11fd638ffd39
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-05-06 09:13:11 -0700

    temporarily comment out imports

--- parkcore_app/test/parking_details_test.dart
+++ parkcore_app/test/parking_details_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:parkcore_app/parking/parking_details.dart';
+//import 'package:flutter/material.dart';
+//import 'package:flutter_test/flutter_test.dart';
+//import 'package:parkcore_app/parking/parking_details.dart';
 //import 'package:parkcore_app/parking/find_parking.dart';
 
 void main() {
