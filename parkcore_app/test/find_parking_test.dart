fe5b73c63d4db8e0e8e3155ca685d153dbce9e2d
Isabel Hernandez <isabelhernandez@isabels-mbp.attlocal.net>
2020-05-13 10:06:08 -0700

    Added Tests

--- parkcore_app/test/find_parking_test.dart
+++ parkcore_app/test/find_parking_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:parkcore_app/parking/find_parking.dart';
 
 void main() {
@@ -13,6 +14,7 @@ void main() {
     final titleFinder = find.text('Find Parking');
     expect(titleFinder, findsOneWidget);
   });
+
   testWidgets('Calculate Distance RaisedButton Field', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
@@ -21,6 +23,15 @@ void main() {
 
     expect(find.widgetWithText(RaisedButton, 'Show distance to CSU, Chico'), findsOneWidget);
   });
+
+  /*testWidgets('Flat Button Field', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(find.byType(FlatButton), findsOneWidget);
+  });*/
 
   testWidgets('Finds Price Filter', (WidgetTester tester) async {
     // Build an app with a Text widget that displays the letter 'H'.
@@ -80,7 +91,6 @@ void main() {
     // Find a widget that displays the letter 'H'.
     expect(find.text("Let us know you're interested!"), findsOneWidget);
   });
-  
 
   testWidgets('Haversize for Distance', (WidgetTester tester) async {
     // Build our app and trigger a frame.
@@ -89,6 +99,24 @@ void main() {
     ));
 
     expect(FindParking().createState().haversize('{39.727580,-121.840430}'), '13');
+  });
+
+  testWidgets('Haversize for Distance', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().haversize('{39.72096140000001,-121.8450891}'), '17');
+  });
+
+  testWidgets('Haversize for Distance', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().haversize('{39.7282242,-121.8408044}'), '12');
   });
 
   testWidgets('Adjust Distance-Less than a Mile', (WidgetTester tester) async {
@@ -100,6 +128,15 @@ void main() {
     expect(FindParking().createState().adjustDistance(.5), .5);
   });
 
+  testWidgets('Adjust Distance-Less than a Mile', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().adjustDistance(.2), .2);
+  });
+
   testWidgets('Adjust Distance-More than a Mile', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
@@ -109,7 +146,17 @@ void main() {
     expect(FindParking().createState().adjustDistance(3.6), 4.35);
   });
 
+  testWidgets('Adjust Distance-More than a Mile', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().adjustDistance(2.5), 3);
+  });
+
   testWidgets('Parking Spot Column', (WidgetTester tester) async {
+    
     final childWidget = Padding(padding: const EdgeInsets.all(10.0), child: Column());
 
     // Provide the childWidget to the Container.
@@ -118,4 +165,35 @@ void main() {
     // Search for the childWidget in the tree and verify it exists.
     expect(find.byWidget(childWidget), findsOneWidget);
   });
+
+  /*testWidgets('Find ListView', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    // expect to find 1 form
+    expect(find.byType(NetworkImage), findsOneWidget);
+  });*/
+
+  /*testWidgets('Calculate Distance RaisedButton Works', (WidgetTester tester) async {
+    
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+    
+    final childWidget = Container(padding: EdgeInsets.all(5.0));
+    
+    expect(FindParking().createState().displayDistance('{39.7285,-121.8375}'), childWidget);
+  
+  });
+  testWidgets('Calculate Distance RaisedButton Field', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(find.byType(Icon), findsWidgets);
+  });*/
+
 }
