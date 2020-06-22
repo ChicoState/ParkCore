903ab3b9aceab57bb9b3487f5221f567ee8c9040
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-06-22 16:53:42 -0700

    adds username & id to CurrentUser

--- parkcore_app/lib/models/CurrentUser.dart
+++ parkcore_app/lib/models/CurrentUser.dart
@@ -3,11 +3,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 class CurrentUser {
   CurrentUser(
       this.currentUser,
+      this.name,
+      this.id,
   );
 
   FirebaseUser currentUser;
+  String name;
+  String id;
 
   Map<String, dynamic> toJson() => {
     'currentUser' : currentUser,
   };
+
+  String getUserName() {
+    if (currentUser != null) {
+      return name;
+    } else {
+      return 'no current user';
+    }
+  }
+
+  String getUserID() {
+    if (currentUser != null) {
+      return id;
+    } else {
+      return 'no current user';
+    }
+  }
 }
