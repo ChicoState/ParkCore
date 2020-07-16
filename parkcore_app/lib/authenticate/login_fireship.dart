3e5fd86bdfc9383a4a5f01e67b5210c3c63ad42f
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2020-07-16 16:44:44 -0700

    commented out unused code

--- parkcore_app/lib/authenticate/login_fireship.dart
+++ parkcore_app/lib/authenticate/login_fireship.dart
@@ -42,7 +42,9 @@ class LoginPage extends StatelessWidget {
                   UserProfile(),
                 ],
               ),
-            )));
+            ),
+        ),
+    );
   }
 }
 
@@ -71,27 +73,27 @@ class UserProfileState extends State<UserProfile> {
   }
 }
 
-class LoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: authService.user,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MaterialButton(
-              onPressed: () => authService.signOut(),
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text('Sign out'),
-            );
-          } else {
-            return MaterialButton(
-              onPressed: () => authService.googleSignIn(),
-              color: Colors.white,
-              textColor: Colors.black,
-              child: Text('Login with Google'),
-            );
-          }
-        });
-  }
-}
+//class LoginButton extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return StreamBuilder(
+//        stream: authService.user,
+//        builder: (context, snapshot) {
+//          if (snapshot.hasData) {
+//            return MaterialButton(
+//              onPressed: () => authService.signOut(),
+//              color: Colors.red,
+//              textColor: Colors.white,
+//              child: Text('Sign out'),
+//            );
+//          } else {
+//            return MaterialButton(
+//              onPressed: () => authService.googleSignIn(),
+//              color: Colors.white,
+//              textColor: Colors.black,
+//              child: Text('Login with Google'),
+//            );
+//          }
+//        });
+//  }
+//}
